ecfb074569f9961596ac0d9b40be9c03a3a3379a
Valentina Khudyakova <khudyakovavi@gmail.com>
2016-12-03 19:14:00 +0300

    добавил бота

--- bot.py
+++ bot.py
@@ -1,8 +1,21 @@
-def get_answer(key, dict):
-	return dict[key.lower()]
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
+def main():
+    updater = Updater("312039735:AAFzeUt-LKAnHjr9n58unpfO8_CAsxbCq70")
+    
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(MessageHandler([Filters.text], talk_to_me))
+    updater.start_polling()
+    updater.idle()
+
+def greet_user(bot, update):
+    print('Вызван /start')
+    bot.sendMessage(update.message.chat_id, text='Давай общаться!')
+
+def talk_to_me(bot, update):
+    print('Пришло сообщение: {}'.format(update.message.text))
+    bot.sendMessage(update.message.chat_id, update.message.text)
 
 if __name__ == '__main__':
-	my_dict = {"name":"me", "voice":"laud"}
-	print(get_answer("voice", my_dict))
-
+    main()
